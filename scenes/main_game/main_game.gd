9bc35c6b4049c995c3b058669ed196145747f3cf
ivo4 <29428624+ivo4@users.noreply.github.com>
2024-10-05 20:36:04 +0300

    powerup button

--- scenes/main_game/main_game.gd
+++ scenes/main_game/main_game.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @onready var annoyance_meter: Node2D = $AnnoyanceMeter
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
---
+++ scenes/main_game/powerup_button.gd
@@ -0,0 +1,33 @@
+@tool
+extends Node2D
+
+@export var max_value: int = 100
+@export var current_value: int:
+	get:
+		return current_value
+	set(value):
+		current_value = clampi(value, 0, max_value)
+@export var button_texture: Texture2D:
+	get:
+		return button_texture
+	set(value):
+		button_texture = value
+
+
+func _ready() -> void:
+	$ButtonProgressMask/TextureButton.disabled = true
+	$ButtonProgressMask/TextureButton.texture_normal = button_texture
+	$GrayBackground.texture = button_texture
+	$ButtonProgressMask.texture.size.x = button_texture.get_size().x
+	$ButtonProgressMask.texture.size.y = button_texture.get_size().y
+
+
+func _process(_delta: float) -> void:
+	$ButtonProgressMask.offset.y = $ButtonProgressMask.texture.size.y
+	var current_offset_percent: float = 100.0 * current_value / max_value
+	$ButtonProgressMask.offset.y = $ButtonProgressMask.texture.size.y - ($ButtonProgressMask.texture.size.y * current_offset_percent / 100)
+	$ButtonProgressMask/TextureButton.disabled = (current_value != max_value)
+
+
+func _on_texture_button_pressed() -> void:
+	print("powerup clicked")
